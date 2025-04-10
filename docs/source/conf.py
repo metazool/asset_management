0b24a839efb0fc1f1536e442c277232ad0501c05
Jo Walsh <metazool@fastmail.net>
2025-04-10 17:36:59 +0100

    Update copyright statements to use real name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,18 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Asset Management System'
-copyright = '2024, Your Organization'
-author = 'Your Organization'
+copyright = '2024, Jo Walsh'
+author = 'Jo Walsh'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -17,6 +25,9 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
